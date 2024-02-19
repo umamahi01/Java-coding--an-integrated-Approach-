dd8abe1b918cb964e95fa724ed5c3f5652bd4f63
umamahi01 <uma102112@gmail.com>
2024-02-19 20:30:01 +0530

    "basic program Fabinocci series"

--- FabinocciSeries.java
+++ FabinocciSeries.java
@@ -2,29 +2,31 @@ import java.io.*;
 
 
 public class FabinocciSeries {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("enter n times = ");
-        String str = br.readLine();
-        int n = Integer.parseInt(str);
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //**creating BufferedReader object br to read InputStreamReader  object that takes input from keyboard(field(in) in System class)*/
+        System.out.println("enter n times = ");//taking input 
+        String str = br.readLine();//BufferedReader object reads input from keyboard with readLine() method remeber to add or handle IOexception at main method signature (throws IOException) basically we add this bcz bufferedReader class throws IOException when read() or readLine() method is used so to handle this runtime error simply throws IOException or handle the IOException with try-catch block
+        int n = Integer.parseInt(str); //String input converted into int by using Integer class parseInt() method
         int f1 = 0;
         int f2 = 1;
         int f = f1+f2;
         System.out.print(f1+" "+ f2+" "+f+" ");
 
+    //    **method-1**
         // for(int i=3;i<n;i++){
         //        f1 = f2;
         //        f2 = f;
         //        f = f1+f2;
         //        System.out.print(f+" ");
         // }
-        int count = 3;
-        while(count < n){
+        //**mthod 2 */ 
+        int count = 3; //varaible starts at 3 bcz we already printed 3 series of numbers 
+        while(count < n){ //so condition starts from 3<n until it reaches a point n<n loop will exit
                f1 = f2;
                f2 = f;
                f= f1+f2;
-               System.out.print(f+" ");
-               count++;
+               System.out.print(f+" "); 
+               count++;  //the count variable should be increment bcz to exit the loop to reach condition n<n if the count variable not incremented this will go infinite loop
         }
 
     }
