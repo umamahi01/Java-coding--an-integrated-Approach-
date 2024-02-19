2b1f408294f65e2038190fe9953394b0ac718acc
umamahi01 <uma102112@gmail.com>
2024-02-19 20:09:30 +0530

    git commit "basic program leap year"

--- Is_LeapyearOrNot.java
+++ Is_LeapyearOrNot.java
@@ -7,10 +7,12 @@ public class Is_LeapyearOrNot {
         System.out.print("enter year:-");
         String str = br.readLine();
         int year = Integer.parseInt(str);
+        // if the input year a century year then it must be divided by 400 if yes print leap year
         if(year%100 == 0 && year%400 == 0){
             System.out.println("leap year");
 
         }
+        //if the input year is not a century year then it must be divided by 4 to prove true for being an leap year if yes print leap year
         else if (year%100 != 0 && year%4 == 0){
             System.out.println("leap year");
 
